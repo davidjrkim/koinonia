4201fc79adf5c715d59c59f5205adaf10b702b8c
David Kim <davidjrkims@gmail.com>
2021-01-01 16:17:27 +0900

    load text added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $('#album').turn({gradients: true, acceleration: true});
 
-alert("책 모서리를 누르거나 잡고 다음 페이지로 넘길수있습니다")
+// alert("책 모서리를 누르거나 잡고 다음 페이지로 넘길수있습니다")
 
 var zoomreset = function() {
     var viewport = document.querySelector("meta[name='viewport']");
